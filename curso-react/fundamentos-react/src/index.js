f306a271b125cf6bbfb1e671f992206e8df037f0
hamiltonmbsilva <hamiltonmb.silva@gmail.com>
2021-07-01 22:47:23 -0300

    Conhecendo jsx

--- curso-react/fundamentos-react/src/index.js
+++ curso-react/fundamentos-react/src/index.js
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
 
-const el = document.getElementById('root')
-ReactDOM.render('Olá React', el)
+ReactDOM.render(
+  <div>
+      <strong>Olá React</strong>    
+  </div> ,
+  document.getElementById('root')
+)
